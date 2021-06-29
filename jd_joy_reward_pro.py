16a1ac8739c308c9a9ff8cefc636074798e471d9
Waikkii <50266226+Waikkii@users.noreply.github.com>
2021-06-30 02:03:11 +0800

    Update jd_joy_reward_pro.py

--- jd_joy_reward_pro.py
+++ jd_joy_reward_pro.py
@@ -12,60 +12,34 @@ cron 59 7,15,23 * * * * 或 0 0,8,16 * * *
 
 import json
 import os
+import sys
 import threading
 import time
 import datetime
-import requests
-
-
-def main(cookie, validate):
-    target = 500 or os.environ.get('JD_JOY_REWARD_NAME')
-    headers = {
-        'Host': 'jdjoy.jd.com',
-        'accept': '*/*',
-        'content-type': 'application/json',
-        'origin': 'https://h5.m.jd.com',
-        "User-Agent": '23',
-        'referer': 'https://jdjoy.jd.com/',
-        'accept-language': 'zh-cn',
-        'cookie': cookie
-    }
-    url = f"https://jdjoy.jd.com/common/gift/getBeanConfigs?reqSource=h5&invokeKey=NRp8OPxZMFXmGkaE&validate={validate}"
-    tasks = requests.get(url, headers=headers).json()
-    h = datetime.datetime.now().hour
-    config = {}
-    if 0 <= h < 8:
-        config = tasks['data']['beanConfigs0']
-    if 8 <= h < 16:
-        config = tasks['data']['beanConfigs8']
-    if 16 <= h < 24:
+	@@ -42,30 +43,26 @@ def main(cookie, validate):
         config = tasks['data']['beanConfigs16']
 
     for bean in config:
-        print(bean['id'], bean['giftName'], bean['leftStock'])
+        sys.stdout.write(f"{bean['id']} {bean['giftName']} {bean['leftStock']}\n")
         if bean['giftValue'] == target:
             while 1:
                 if datetime.datetime.now().second == 0:
                     break
                 time.sleep(0.1)
-            print('exchange()')
+            sys.stdout.write('exchange()\n')
             url = f"https://jdjoy.jd.com/common/gift/new/exchange?reqSource=h5&invokeKey=NRp8OPxZMFXmGkaE&validate={validate}"
             data = {"buyParam": {"orderSource": 'pet', "saleInfoId": bean['id']}, "deviceInfo": {}}
             res = requests.post(url, headers=headers, data=json.dumps(data)).json()
-            print(res, end='')
+            sys.stdout.write(json.dumps(res) + '\n')
             if res['errorCode'] == 'buy_success':
-                print(f"cookie{cookie.split('pt_pin=')[1].replace(';', '')}兑换成功")
+                sys.stdout.write(f"cookie{cookie.split('pt_pin=')[1].replace(';', '')}兑换成功\n")
     lock.release()
 
 
 if __name__ == '__main__':
     print("🔔宠汪汪兑换Pro,开始！")
-    cookies = [
-        # 'ck1',
-        # 'ck2',
-        # 'ck3',
-        # ...
-    ]
+    # cookies = ['ck1','ck2','ck3']
+    cookies = []
     if os.environ.get("JOYPRO_JD_COOKIE"):
         cookies.append(os.environ.get("JOYPRO_JD_COOKIE").split('&'))
     lock = threading.BoundedSemaphore(20)
