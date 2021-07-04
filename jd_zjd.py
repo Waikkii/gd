61cf916e06edaea38d717fd70aee429736d08816
Waikkii <50266226+Waikkii@users.noreply.github.com>
2021-07-04 16:48:09 +0800

    Update jd_zjd.py

--- jd_zjd.py
+++ jd_zjd.py
@@ -1,42 +1,54 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
 '''
-项目名称: JD-Script / jd_zjd 
+项目名称: JD-Script / jd_qjd
 Author: Curtin
-功能：微信小程序-赚京豆-瓜分10亿京豆自动助力，默认给账号1助力，多账号才能玩~
-Date: 2021/6/25 下午9:16
+功能：全民抢京豆（7.2-7.15）：https://h5.m.jd.com/rn/3MQXMdRUTeat9xqBSZDSCCAE9Eqz/index.html?has_native=0
+    满160豆需要20人助力，每个用户目前只能助力2次不同的用户。
+Date: 2021/7/3 上午10:02
 TG交流 https://t.me/topstyle996
 TG频道 https://t.me/TopStyle2021
-updateTime: 2021.7.1 11:22
+update: 2021.7.4 08:36
+* 修复了助力活动不存在、增加了随机UA（如果未定义ua则启用随机UA）
+
 '''
 
-#####
 #ck 优先读取【JDCookies.txt】 文件内的ck  再到 ENV的 变量 JD_COOKIE='ck1&ck2' 最后才到脚本内 cookies=ck
-cookies=''
-#助力账号，填写pt_pin或用户名的值，如 zlzh = ['aaaa','xxxx','yyyy'] ,支持ENV export zlzh=['CurtinLV','xxxx','yyyy']
-zlzh = ['Curtinlv', '买买买', '东哥']
-#####
+cookies = ''
+qjd_zlzh = ['Your JD_User', '买买买']
 
-
+# 建议调整一下的参数
+# UA 可自定义你的，注意格式: jdapp;iPhone;10.0.4;13.1.1;93b4243eeb1af72d142991d85cba75c66873dca5;network/wifi;ADID/8679C062-A41A-4A25-88F1-50A7A3EEF34A;model/iPhone13,1;addressid/3723896896;appBuild/167707;jdSupportDarkMode/0
+UserAgent = ''
+# 限制速度 （秒）
+sleepNum = 0.1
 
 import os, re
+import random, string
 try:
     import requests
 except Exception as e:
     print(e, "\n缺少requests 模块，请执行命令安装：python3 -m pip install requests")
     exit(3)
-from urllib.parse import unquote
+from urllib.parse import unquote, quote
 import json
 import time
 requests.packages.urllib3.disable_warnings()
+
+ss = requests.session()
+
 pwd = os.path.dirname(os.path.abspath(__file__)) + os.sep
 t = time.time()
 aNum = 0
 beanCount = 0
+userCount = {}
+
 class getJDCookie(object):
     # 适配各种平台环境ck
     def getckfile(self):
-        if os.path.exists('/ql/config/env.sh'):
+        if os.path.exists(pwd + 'JDCookies.txt'):
+            return pwd + 'JDCookies.txt'
+        elif os.path.exists('/ql/config/env.sh'):
             print("当前环境青龙面板新版")
             return '/ql/config/env.sh'
         elif os.path.exists('/ql/config/cookie.sh'):
@@ -47,8 +59,8 @@ class getJDCookie(object):
             return '/jd/config/config.sh'
         elif os.path.exists(pwd + 'JDCookies.txt'):
             return pwd + 'JDCookies.txt'
-        else:
-            return pwd + 'JDCookies.txt'
+        return pwd + 'JDCookies.txt'
+
     # 获取cookie
     def getCookie(self):
         global cookies
@@ -62,15 +74,21 @@ class getJDCookie(object):
                     r = re.compile(r"pt_key=.*?pt_pin=.*?;", re.M | re.S | re.I)
                     cks = r.findall(cks)
                     if len(cks) > 0:
+                        if 'JDCookies.txt' in ckfile:
+                            print("当前获取使用 JDCookies.txt 的cookie")
                         cookies = ''
                         for i in cks:
                             cookies += i
+                        return
             else:
                 with open(pwd + 'JDCookies.txt', "w", encoding="utf-8") as f:
                     cks = "#多账号换行，以下示例：（通过正则获取此文件的ck，理论上可以自定义名字标记ck，也可以随意摆放ck）\n账号1【Curtinlv】cookie1;\n账号2【TopStyle】cookie2;"
                     f.write(cks)
                     f.close()
-                pass
+            if "JD_COOKIE" in os.environ:
+                if len(os.environ["JD_COOKIE"]) > 10:
+                    cookies = os.environ["JD_COOKIE"]
+                    print("已获取并使用Env环境 Cookie")
         except Exception as e:
             print(f"【getCookie Error】{e}")
 
@@ -137,141 +155,141 @@ class getJDCookie(object):
         else:
             print("cookie 格式错误！...本次操作已退出")
             exit(4)
-
-
-
-# 获取系统ENV环境参数优先使用 适合Ac、云服务等环境
-# JD_COOKIE=cookie （多账号&分隔）
-if "JD_COOKIE" in os.environ:
-    if len(os.environ["JD_COOKIE"]) > 10:
-        cookies = os.environ["JD_COOKIE"]
-        print("已获取并使用Env环境 Cookie")
-if "zlzh" in os.environ:
-    if len(os.environ["zlzh"]) > 1:
-        zlzh = os.environ["zlzh"]
-        zlzh = zlzh.replace('[', '').replace(']', '').replace('\'', '').replace(' ', '').split(',')
-        print("已获取并使用Env环境 zlzh:", zlzh)
-
 getCk = getJDCookie()
 getCk.getCookie()
 
-# 开启助力任务
-def starAssist(sid, headers):
+if "qjd_zlzh" in os.environ:
+    if len(os.environ["qjd_zlzh"]) > 1:
+        qjd_zlzh = os.environ["qjd_zlzh"]
+        qjd_zlzh = qjd_zlzh.replace('[', '').replace(']', '').replace('\'', '').replace(' ', '').split(',')
+        print("已获取并使用Env环境 qjd_zlzh:", qjd_zlzh)
+
+def userAgent():
+    """
+    随机生成一个UA
+    :return:
+    """
+    if not UserAgent:
+        uuid = ''.join(random.sample('123456789abcdef123456789abcdef123456789abcdef123456789abcdef', 40))
+        iosVer = ''.join(random.sample(["14.5.1", "14.4", "14.3", "14.2", "14.1", "14.0.1", "13.7", "13.1.2", "13.1.1"], 1))
+        iPhone = ''.join(random.sample(["8", "9", "10", "11", "12", "13"], 1))
+        return f'jdapp;iPhone;10.0.4;{iosVer};{uuid};network/wifi;ADID/8679C062-A41A-4A25-88F1-50A7A3EEF34A;model/iPhone{iPhone},1;addressid/3723896896;appBuild/167707;jdSupportDarkMode/0'
+    else:
+        return UserAgent
+
+def getShareCode(ck):
     global aNum
     try:
-        timestamp = int(round(t * 1000))
-        url = 'https://api.m.jd.com/api?functionId=vvipclub_distributeBean_startAssist&body={%22activityIdEncrypted%22:%22' + sid + '%22,%22channel%22:%22FISSION_BEAN%22}&appid=swat_miniprogram&client=tjj_m&screen=1920*1080&osVersion=5.0.0&networkType=wifi&sdkName=orderDetail&sdkVersion=1.0.0&clientVersion=3.1.3&area=11&fromType=wxapp&timestamp=' + str(timestamp)
-        requests.get(url=url, headers=headers, verify=False, timeout=30).json()
+        v_num1 = ''.join(random.sample(["1", "2", "3", "4", "5", "6", "7", "8", "9"], 1)) + ''.join(random.sample(string.digits, 4))
+        url = 'https://api.m.jd.com/client.action?functionId=signBeanGroupStageIndex&body=%7B%22monitor_refer%22%3A%22%22%2C%22rnVersion%22%3A%223.9%22%2C%22fp%22%3A%22-1%22%2C%22shshshfp%22%3A%22-1%22%2C%22shshshfpa%22%3A%22-1%22%2C%22referUrl%22%3A%22-1%22%2C%22userAgent%22%3A%22-1%22%2C%22jda%22%3A%22-1%22%2C%22monitor_source%22%3A%22bean_m_bean_index%22%7D&appid=ld&client=apple&clientVersion=&networkType=&osVersion=&uuid=&jsonp=jsonp_' + str(int(round(t * 1000))) + '_' + v_num1
+        head = {
+            'Cookie': ck,
+            'Accept': '*/*',
+            'Connection': 'keep-alive',
+            'Referer': 'https://h5.m.jd.com/rn/3MQXMdRUTeat9xqBSZDSCCAE9Eqz/index.html?has_native=0',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Host': 'api.m.jd.com',
+            # 'User-Agent': 'Mozilla/5.0 (iPhone CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Mobile/15E148 Safari/604.1',
+            'User-Agent': userAgent(),
+            'Accept-Language': 'zh-cn'
+        }
+        requests.get(url='https://h5.m.jd.com/rn/3MQXMdRUTeat9xqBSZDSCCAE9Eqz/index.html?has_native=0&tttparams=&lng=&lat=&sid=&un_area=',  headers=head, verify=False, timeout=30)
+        resp = requests.get(url=url, headers=head, verify=False, timeout=30).text
+        r = re.compile(r'jsonp_.*?\((.*?)\)\;', re.M | re.S | re.I)
+        result = r.findall(resp)
+        jsonp = json.loads(result[0])
+        try:
+            groupCode = jsonp['data']['groupCode']
+            shareCode = jsonp['data']['shareCode']
+            activityId = jsonp['data']['activityMsg']['activityId']
+            sumBeanNumStr = int(jsonp['data']['sumBeanNumStr'])
+        except:
+            if aNum < 5:
+                aNum += 1
+                return getShareCode(ck)
+            else:
+                groupCode = 0
+                shareCode = 0
+                sumBeanNumStr = 0
+                aNum = 0
+                activityId = 0
         aNum = 0
+        return groupCode, shareCode, sumBeanNumStr, activityId
     except Exception as e:
-        if aNum < 5:
-            aNum += 1
-            return starAssist(sid, headers)
-        else:
-            print("starAssist Error", e)
-            exit(1)
+        print(f"getShareCode Error", e)
 
-
-#获取助力码
-def getShareCode(headers):
-    global assistStartRecordId, encPin, sid, aNum
+def helpCode(ck, groupCode, shareCode,u, unum, user, activityId):
     try:
-        url = f'https://api.m.jd.com/api?functionId=distributeBeanActivityInfo&fromType=wxapp&timestamp={int(round(t * 1000))}'
-        body = 'body=%7B%22paramData%22%3A%7B%22channel%22%3A%22FISSION_BEAN%22%7D%7D&appid=swat_miniprogram&client=tjj_m&screen=1920*1080&osVersion=5.0.0&networkType=wifi&sdkName=orderDetail&sdkVersion=1.0.0&clientVersion=3.1.3&area=11'
-        responses = requests.post(url, headers=headers, data=body, verify=False, timeout=30).json()
-        if responses['success']:
-            data = responses['data']
-            sid = data['id']
-            encPin = data['encPin']
-            try:
-                assistStartRecordId = data['assistStartRecordId']
-            except:
-                starAssist(sid, header)
-                return getShareCode(headers)
-            aNum = 0
-            return assistStartRecordId, encPin, sid
-    except Exception as e:
-        if aNum < 5:
-            aNum += 1
-            return getShareCode(headers)
+        v_num1 = ''.join(random.sample(["1", "2", "3", "4", "5", "6", "7", "8", "9"], 1)) + ''.join(random.sample(string.digits, 4))
+        headers = {
+            'Cookie': ck,
+            'Accept': '*/*',
+            'Connection': 'keep-alive',
+            'Referer': f'https://h5.m.jd.com/rn/42yjy8na6pFsq1cx9MJQ5aTgu3kX/index.html?jklActivityId=115&source=SignSuccess&jklGroupCode={groupCode}&ad_od=1&jklShareCode={shareCode}',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Host': 'api.m.jd.com',
+            'User-Agent': userAgent(),
+            'Accept-Language': 'zh-cn'
+        }
+        url = 'https://api.m.jd.com/client.action?functionId=signGroupHelp&body=%7B%22activeType%22%3A2%2C%22groupCode%22%3A%22' + str(groupCode) + '%22%2C%22shareCode%22%3A%22' + shareCode + f'%22%2C%22activeId%22%3A%22{activityId}%22%2C%22source%22%3A%22guest%22%7D&appid=ld&client=apple&clientVersion=10.0.4&networkType=wifi&osVersion=13.7&uuid=&openudid=&jsonp=jsonp_{int(round(t * 1000))}_{v_num1}'
+        resp = requests.get(url=url, headers=headers, verify=False, timeout=30).text
+        r = re.compile(r'jsonp_.*?\((.*?)\)\;', re.M | re.S | re.I)
+        result = r.findall(resp)
+        jsonp = json.loads(result[0])
+        helpToast = jsonp['data']['helpToast']
+        pageFlag = jsonp['data']['pageFlag']
+        if pageFlag == 0:
+            print(f"账号{unum}【{u}】助力失败! 原因：{helpToast}")
+            if '满' in helpToast:
+                print(f"## 恭喜账号【{user}】团已满，今日累计获得160豆")
+                return True
+            return False
         else:
-            print("getShareCode Error", e)
-            exit(2)
+            if '火' in helpToast:
+                print(f"账号{unum}【{u}】助力失败! 原因：{helpToast}")
+            else:
+                print(f"账号{unum}【{u}】{helpToast} , 您也获得1豆哦~")
+            return False
+    except Exception as e:
+        print(f"helpCode Error ", e)
 
-#设置请求头
-def setHeaders(cookie):
-    headers = {
-        'Cookie': cookie,
-        'content-type': 'application/x-www-form-urlencoded',
-        'Connection': 'keep-alive',
-        'Accept-Encoding': 'gzip,compress,br,deflate',
-        'Referer': 'https://servicewechat.com/wxa5bf5ee667d91626/148/page-frame.html',
-        'Host': 'api.m.jd.com',
-        'User-Agent': 'Mozilla/5.0 (iPhone CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.7(0x1800072d) NetType/WIFI Language/zh_CN'
-    }
-
-    return headers
-
-def assist(ck, sid, eid, aid, user, name, a):
-    global beanCount
-    timestamp = int(round(t * 1000))
-    headers = {
-        'Cookie': ck + 'wxclient=gxhwx;ie_ai=1;',
-        'Accept': '*/*',
-        'Connection': 'keep-alive',
-        'Referer': 'https://servicewechat.com/wxa5bf5ee667d91626/148/page-frame.html',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Host': 'api.m.jd.com',
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.1(0x1800012a) NetType/WIFI Language/zh_CN',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-cn'
-    }
-    url = 'https://api.m.jd.com/api?functionId=vvipclub_distributeBean_assist&body=%7B%22activityIdEncrypted%22:%22' + sid + '%5Cn%22,%22assistStartRecordId%22:%22' + str(aid) + '%22,%22assistedPinEncrypted%22:%22' + eid + '%5Cn%22,%22channel%22:%22FISSION_BEAN%22%7D&appid=swat_miniprogram&client=tjj_m&screen=1920*1080&osVersion=5.0.0&networkType=wifi&sdkName=orderDetail&sdkVersion=1.0.0&clientVersion=3.1.3&area=1_72_4137_0&fromType=wxapp&timestamp=' + str(timestamp)
-    resp = requests.get(url, headers=headers, verify=False, timeout=30).json()
-    if resp['success']:
-        print(f"用户{a}【{user}】助力【{name}】成功~")
-        if resp['data']['assistedNum'] == 4:
-            beanCount += 80
-            print(f"{name}, 恭喜获得8毛京豆，以到账为准。")
-            print("## 开启下一轮助力")
-            starAssist(sid, header)
-            getShareCode(header)
-    else:
-        print(f"用户{a}【{userNameList[a-1]}】没有助力次数了。")
-
-
-
-
-#开始互助
 def start():
-    global header,cookiesList, userNameList, pinNameList
-    print("微信小程序-赚京豆-瓜分助力")
+    print("### 全民抢京豆-助力 ###")
+    global cookiesList, userNameList, pinNameList, ckNum, beanCount, userCount
     cookiesList, userNameList, pinNameList = getCk.iscookie()
-    for ckname in zlzh:
+    for ckname in qjd_zlzh:
         try:
             ckNum = userNameList.index(ckname)
         except Exception as e:
             try:
                 ckNum = pinNameList.index(ckname)
             except:
-                print("请检查助力账号名称是否正确？提示：助力名字可填pt_pin的值、也可以填用户名。")
-                exit(9)
+                print(f"请检查被助力账号【{ckname}】名称是否正确？提示：助力名字可填pt_pin的值、也可以填账号名。")
+                continue
 
         print(f"### 开始助力账号【{userNameList[int(ckNum)]}】###")
+        groupCode, shareCode, sumBeanNumStr, activityId = getShareCode(cookiesList[ckNum])
+        if groupCode == 0:
+            print(f"## {userNameList[int(ckNum)]}  获取互助码失败。请稍后再试！")
+            continue
+        u = 0
+        for i in cookiesList:
+            if i == cookiesList[ckNum]:
+                u += 1
+                continue
+            result = helpCode(i, groupCode, shareCode, userNameList[u], u+1, userNameList[int(ckNum)], activityId)
+            time.sleep(sleepNum)
+            if result:
+                break
+            u += 1
+        groupCode, shareCode, sumBeanNumStr, activityId = getShareCode(cookiesList[ckNum])
+        userCount[f'{userNameList[ckNum]}'] = sumBeanNumStr
+        beanCount += sumBeanNumStr
+    print("\n-------------------------")
+    for i in userCount.keys():
+        print(f"账号【{i}】已抢京豆: {userCount[i]}")
+    print(f"## 今日累计获得 {beanCount} 京豆")
 
-        header = setHeaders(cookiesList[int(ckNum)])
-        getShareCode(header)
-        starAssist(sid, header)
-        getShareCode(header)
-        a = 1
-        for i, name in zip(cookiesList, userNameList):
-            if a == ckNum+1:
-                a += 1
-            else:
-                assist(i, sid, encPin, assistStartRecordId, name, userNameList[int(ckNum)], a)
-                a += 1
-        if beanCount > 0:
-            print(f'\n### 本次累计获得{beanCount}京豆')
 
 if __name__ == '__main__':
     start()
