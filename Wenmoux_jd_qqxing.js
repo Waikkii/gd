52fbde3ed14f57b794bda611943a026fdb901f14
Waikkii <50266226+Waikkii@users.noreply.github.com>
2021-07-08 02:09:57 +0800

    Update Wenmoux_jd_qqxing.js

--- Wenmoux_jd_qqxing.js
+++ Wenmoux_jd_qqxing.js
@@ -94,8 +94,8 @@ $.shareuuid = "5e81094ee1d640b2996883b48d0c410a"
                     for (k = 0; k < $.drawchance; k++) {
                         await draw()
                     }
-                    let exchanges =Math.floor($.foodNum/1000)
-                    console.log(`可兑换 ${exchanges} 次 20京🐶`)
+                    let exchanges =Math.floor($.foodNum/3000)
+                    console.log(`可兑换 ${exchanges} 次 50京🐶`)
                     for(q = 0;q<exchanges && Exchange;q++){
                     await exchange(14)   
                     }
