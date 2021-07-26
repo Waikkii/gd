4303e7fd3780f6fbe90ed5f2f61594aa41263eee
Waikkii <50266226+Waikkii@users.noreply.github.com>
2021-07-26 20:48:25 +0800

    Update angryCash.js

--- secret/angryCash.js
+++ secret/angryCash.js
@@ -146,7 +146,7 @@ function requestApi(functionId, cookie, body = {}, tool) {
 function requireConfig() {
     return new Promise(resolve => {
         notify = $.isNode() ? require('./sendNotify') : '';
-        const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+        const jdCookieNode = $.isNode() ? require('./jdbackupCOOKIE.js') : '';
         if ($.isNode()) {
             Object.keys(jdCookieNode).forEach((item) => {
                 if (jdCookieNode[item]) {
