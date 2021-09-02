f5e0b9c972564ff27bc4f241252cb47e466e4f54
Waikiki <50266226+Waikkii@users.noreply.github.com>
2021-09-02 17:34:01 +0800

    Update jd_ql_refreshck.py

--- jd_ql_refreshck.py
+++ jd_ql_refreshck.py
@@ -51,7 +51,7 @@ def wstopt(wskey):
     try:
         url = "http://cdn.xia.me/getck"
         headers = {
-            "Host":"signer.nz.lu",
+            "Host": "signer.nz.lu",
             "user-agent": "Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
         }
         data = {"wskey": wskey, "key": "xb3z4z2m3n847"}
@@ -101,27 +101,30 @@ if __name__ == '__main__':
     wskeys = getitem("JD_WSCK")
     count = 1
     for i in wskeys:
-        if i["status"]==0:
+        if i["status"] == 0:
             ptck = wstopt(i["value"])
-            wspin = re.findall(r"pin=(.*?);", i["value"])[0]
-            if ptck == "wskey错误":
-                print("第%s个wskey可能过期了,pin为%s" % (count, wspin))
-            elif ptck == "未知错误":
-                print("第%s个wskey发生了未知错误,pin为%s" % (count, wspin))
-            else:
-                ptpin = re.findall(r"pt_pin=(.*?);", ptck)[0]
-                item = getckitem("pt_pin=" + ptpin)
-                if item != []:
-                    qlid = item["_id"]
-                    if update(ptck, qlid):
-                        print("第%s个wskey更新成功,pin为%s" % (count, wspin))
-                    else:
-                        print("第%s个wskey更新失败,pin为%s" % (count, wspin))
+            try:
+                wspin = re.findall(r"pin=(.*?);", i["value"])[0]
+                if ptck == "wskey错误":
+                    print("第%s个wskey可能过期了,pin为%s" % (count, wspin))
+                elif ptck == "未知错误" or ptck == "error":
+                    print("第%s个wskey发生了未知错误,pin为%s" % (count, wspin))
                 else:
-                    if insert(ptck):
-                        print("第%s个wskey添加成功" % count)
+                    ptpin = re.findall(r"pt_pin=(.*?);", ptck)[0]
+                    item = getckitem("pt_pin=" + ptpin)
+                    if item != []:
+                        qlid = item["_id"]
+                        if update(ptck, qlid):
+                            print("第%s个wskey更新成功,pin为%s" % (count, wspin))
+                        else:
+                            print("第%s个wskey更新失败,pin为%s" % (count, wspin))
                     else:
-                        print("第%s个wskey添加失败" % count)
+                        if insert(ptck):
+                            print("第%s个wskey添加成功" % count)
+                        else:
+                            print("第%s个wskey添加失败" % count)
+            except:
+                print("第%s个wskey出现异常错误" % count)
             count += 1
         else:
             print("有一个wskey被禁用了")
