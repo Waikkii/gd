62ab2aeb41faffe5fd60b0fd15820a88dad929b4
Waikiki <50266226+Waikkii@users.noreply.github.com>
2021-09-20 12:51:33 +0800

    Update code.sh

--- code.sh
+++ code.sh
@@ -78,7 +78,7 @@ UpdateType="1"
 ## 定义是否自动安装或修复缺失的依赖，默认为1，表示自动修复；留空或其他数值表示不修复。
 FixDependType="1"
 ## 定义监控修复的依赖名称
-package_name="canvas png-js date-fns axios crypto-js ts-md5 tslib @types/node dotenv typescript fs require tslib"
+package_name="canvas png-js crypto-js form-data got"
 
 ## 需组合的环境变量列表，env_name需要和var_name一一对应，如何有新活动按照格式添加(不懂勿动)
 env_name=(
