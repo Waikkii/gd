abec104ff9be6e6564d95d8d296ae5ff84eba49d
Waikiki <50266226+Waikkii@users.noreply.github.com>
2021-09-29 23:33:38 +0800

    勿用！

--- jdFactoryShareCodes.js
+++ jdFactoryShareCodes.js
@@ -8,14 +8,15 @@ let shareCodes = [
   '',//账号二的好友shareCode
 ]
 let CKNumber = ''
-if (process.env.DDFACTORY_SHARECODES) {
-  CKNumber = process.env.DDFACTORY_SHARECODES.split('&')[0];
-  shareCodes = process.env.DDFACTORY_SHARECODES.split('&')[1];
-} else {
-  console.log(`由于您环境变量(DDFACTORY_SHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
-}
+CKNumber = process.env.JD_COOKIE.split('&').length;
+console.log(CKNumber)
 
 for (let i = 0; i < Number(CKNumber); i++) {
-  const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['shareCodes' + index] = shareCodes;
+  if (process.env["DDFACTORY_SHARECODES"+i.toString()]) {
+    shareCodes = process.env["DDFACTORY_SHARECODES"+i.toString()];
+    const index = (i + 1 === 1) ? '' : (i + 1);
+    exports['shareCodes' + index] = shareCodes;
+  } else {
+    break
+  }
 }
