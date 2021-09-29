55b62430dbfaa016e3a01e7780a338c0710f020b
Waikiki <50266226+Waikkii@users.noreply.github.com>
2021-09-29 15:08:42 +0800

    Update jd_sgmh.js

--- jd_sgmh.js
+++ jd_sgmh.js
@@ -273,11 +273,9 @@ function requireConfig() {
     }
     $.shareCodesArr = [];
     if ($.isNode()) {
-      Object.keys(shareCodes).forEach((item) => {
-        if (shareCodes) {
-          $.shareCodesArr.push(shareCodes)
-        }
-      })
+      for (let i = 0; i < Number(CKNumber); i++) {
+        $.shareCodesArr.push(shareCodes);
+      }
     }
     console.log(`您提供了${$.shareCodesArr.length}个账号的${$.name}助力码\n`);
     resolve()
