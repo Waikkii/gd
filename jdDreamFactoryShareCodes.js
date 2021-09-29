1428fb97b86908771111e0977ef8ad98661734ea
Waikiki <50266226+Waikkii@users.noreply.github.com>
2021-09-29 23:34:46 +0800

    Update jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -8,17 +8,17 @@ let shareCodes = [
   '',//账号二的好友shareCode
 ]
 let CKNumber = ''
-if (process.env.DREAM_FACTORY_SHARE_CODES) {
-  CKNumber = process.env.DREAM_FACTORY_SHARE_CODES.split('&')[0];
-  shareCodes = process.env.DREAM_FACTORY_SHARE_CODES.split('&')[1];
-} else {
-  console.log(`由于您环境变量(DREAM_FACTORY_SHARE_CODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
-}
+CKNumber = process.env.JD_COOKIE.split('&').length;
+console.log(CKNumber)
 
 for (let i = 0; i < Number(CKNumber); i++) {
-  const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['shareCodes' + index] = shareCodes;
+  if (process.env["DREAM_FACTORY_SHARE_CODES"+i.toString()]) {
+    shareCodes = process.env["DREAM_FACTORY_SHARE_CODES"+i.toString()];
+    const index = (i + 1 === 1) ? '' : (i + 1);
+    exports['shareCodes' + index] = shareCodes;
+  } else {
+    break
+  }
 }
-
 
 
