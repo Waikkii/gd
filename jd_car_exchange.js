2fd16841207a9b892e80388c2d6d0b3edfb9703d
Waikiki <50266226+Waikkii@users.noreply.github.com>
2021-10-08 01:10:12 +0800

    勿用！

--- jd_car_exchange.js
+++ jd_car_exchange.js
@@ -33,9 +33,9 @@ const JD_API_HOST = 'https://car-member.jd.com/api/';
     for(let i = 0; i < cookiesArr.length; i++){
         if(cookiesArr[i]){
           
-            var ran = Math.round(Math.random()*20);
-            console.log(`为ninja延时`+ran+`秒`);
-            await $.wait(1000*ran);
+            var ran = Math.round(Math.random()*20000);
+            console.log(`为ninja延时`+ran+`毫秒`);
+            await $.wait(ran);
 
             const got = require('got');
             const body = await got('http://localhost:5701/api/users').json();
